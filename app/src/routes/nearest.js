bd69fc6aab8cd7a5fbf59cf24b6f44bcd44065fc
Joshua Phipps <josh.e.phipps@gmail.com>
2019-09-23 22:28:58 +0900

    remove console logs

--- app/src/routes/nearest.js
+++ app/src/routes/nearest.js
@@ -4,19 +4,15 @@ const logfmt = require("logfmt");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  console.log(req.body);
   if (!req.body.coordinates) {
     return res.status(422).json({ error: "Missing coordinates" });
   }
-  console.log(req.body.coordinates)
 
   const osrm = req.app.get("osrm");
   const options = {
     coordinates: req.body.coordinates,
-    // coordinates: [[-81.54729, 28.28113]],
     number: req.body.number || 1
   };
-  console.log(options);
 
   try {
     osrm.nearest(options, (err, result) => {
